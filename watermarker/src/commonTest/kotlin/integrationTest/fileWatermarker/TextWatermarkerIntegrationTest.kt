dcd90e09e988e79f04cd0e0ab4692060a7e9e7cb
Hendrik Norkowski <hendrik.norkowski@isst.fraunhofer.de>
2024-03-12 12:03:48 +0100

    test(TextWatermarker): create watermark insertion and extraction equality integration test cases

---
+++ watermarker/src/commonTest/kotlin/integrationTest/fileWatermarker/TextWatermarkerIntegrationTest.kt
@@ -0,0 +1,76 @@
+package integrationTest.fileWatermarker
+
+import de.fraunhofer.isst.trend.watermarker.SupportedFileType
+import de.fraunhofer.isst.trend.watermarker.fileWatermarker.TextWatermark
+import de.fraunhofer.isst.trend.watermarker.fileWatermarker.TextWatermarker
+import de.fraunhofer.isst.trend.watermarker.files.TextFile
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class TextWatermarkerIntegrationTest {
+    private val textWatermarker = TextWatermarker.default()
+
+    @Test
+    fun watermarkInsertionExtraction_successiveSpaces_equality() {
+        // Arrange
+        val text =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
+                "incididunt ut labore et dolore magna aliqua. Tempus imperdiet nulla malesuada " +
+                "pellentesque elit. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl" +
+                ". Viverra accumsan in  nisl  nisi.  Condimentum  lacinia quis vel eros donec. " +
+                "Urna neque viverra justo"
+        val watermark = "Hello World"
+        val textFile = TextFile.fromString(text)
+        val parsedWatermark = TextWatermark.fromText(watermark)
+
+        // Add watermark
+        // Act
+        val status = textWatermarker.addWatermark(textFile, parsedWatermark)
+
+        // Assert
+        assertTrue(status.isSuccess)
+
+        // Extract Watermark
+        // Act
+        val watermarks = textWatermarker.getWatermarks(textFile)
+
+        // Assert
+        assertTrue(watermarks.isSuccess)
+        assertEquals(watermarks.value!!.size, 1)
+        val firstWatermark = watermarks.value!![0].getText().value!!
+        assertEquals(watermark, firstWatermark)
+    }
+
+    @Test
+    fun watermarkInsertionExtraction_loremIpsum_equality() {
+        // Arrange
+        val text =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
+                "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
+                "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu " +
+                "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+                "culpa qui officia deserunt mollit anim id est laborum."
+        val watermark = "Hello World"
+        val textFile = TextFile.fromString(text)
+        val parsedWatermark = TextWatermark.fromText(watermark)
+
+        // Add watermark
+        // Act
+        val status = textWatermarker.addWatermark(textFile, parsedWatermark)
+
+        // Assert
+        assertTrue(status.isSuccess)
+
+        // Extract Watermark
+        // Act
+        val watermarks = textWatermarker.getWatermarks(textFile)
+
+        // Assert
+        assertTrue(watermarks.isSuccess)
+        assertEquals(watermarks.value!!.size, 1)
+        val firstWatermark = watermarks.value!![0].getText().value!!
+        assertEquals(watermark, firstWatermark)
+    }
+}
