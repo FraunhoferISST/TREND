9a6e0cc24159daeb733da9899fa6664d26f64891
Hendrik Norkowski <hendrik.norkowski@isst.fraunhofer.de>
2024-05-17 16:36:57 +0200

    fix(CRC32): add missing license header

--- watermarker/src/commonMain/kotlin/helper/CRC32.kt
+++ watermarker/src/commonMain/kotlin/helper/CRC32.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023-2024 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ *
+ * This work is licensed under the Fraunhofer License (on the basis of the MIT license)
+ * that can be found in the LICENSE file.
+ */
 package de.fraunhofer.isst.trend.watermarker.helper
 
 expect object CRC32 {
