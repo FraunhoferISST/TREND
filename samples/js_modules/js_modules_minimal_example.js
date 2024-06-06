8159c2d441dc86e1f13b447604f356dfaf444325
Hendrik Norkowski <hendrik.norkowski@isst.fraunhofer.de>
2024-06-06 12:31:06 +0200

    fix(samples): update outdated JS examples (#72)

--- samples/js_modules/js_modules_minimal_example.js
+++ samples/js_modules/js_modules_minimal_example.js
@@ -7,23 +7,24 @@
 import trend from "../../watermarker/build/dist/js/productionExecutable/watermarker.js";
 
 const watermarker = trend.de.fraunhofer.isst.trend.watermarker;
-const TextFile = watermarker.files.TextFile;
-const TextWatermarker =
-  watermarker.fileWatermarker.TextWatermarker.Companion.default();
-const TextWatermark = watermarker.fileWatermarker.TextWatermark;
+const Watermarker = new watermarker.Watermarker();
+const TextWatermark = watermarker.watermarks.TextWatermark;
 
-let textFile = TextFile.Companion.fromString(
-  "Lorem ipsum dolor sit amet, consetetur sadipscing\
-elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam \
-voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no \
-sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur \
-sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, \
-sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd \
-gubergren, no sea takimata sanctus est Lorem ipsum"
-);
+let text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor \
+invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et \
+justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum \
+dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod \
+tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam \
+et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem \
+ipsum.";
 
-let watermark = TextWatermark.Companion.fromText("Hello World!");
+let watermark = TextWatermark.Companion.new("Hello World!");
 
-TextWatermarker.addWatermark(textFile, watermark);
+let result = Watermarker.textAddTrendmarkBuilder(text, watermark);
 
-console.log(textFile.content);
+if (result.isSuccess) {
+  console.log(result.value);
+} else {
+  console.log(result);
+}
+
