81aca27b864baaef386eb800c75646cf33019299
Joris Schiphorst <joris.schiphorst@isst.fraunhofer.de>
2025-02-13 13:15:30 +0100

    feat(watermarker): add static frequency analysis to Watermark

--- watermarker/src/commonMain/kotlin/watermarks/Watermark.kt
+++ watermarker/src/commonMain/kotlin/watermarks/Watermark.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ * Copyright (c) 2023-2025 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
  *
  * This work is licensed under the Fraunhofer License (on the basis of the MIT license)
  * that can be found in the LICENSE file.
@@ -7,15 +7,59 @@
 package de.fraunhofer.isst.trend.watermarker.watermarks
 
 import de.fraunhofer.isst.trend.watermarker.helper.toHexString
+import de.fraunhofer.isst.trend.watermarker.returnTypes.Event
+import de.fraunhofer.isst.trend.watermarker.returnTypes.Result
 import kotlin.js.JsExport
 
 @JsExport
 open class Watermark(var watermarkContent: List<Byte>) {
     companion object {
+        const val SOURCE = "Watermark"
+
         /** Creates a Watermark from [text] */
         fun fromString(text: String): Watermark {
             val bytes = text.encodeToByteArray().asList()
             return Watermark(bytes)
+        }
+
+        /**
+         *  Returns only the most frequent [Watermark] from [watermarks].
+         *  Returns a Warning if more than one most frequent Watermark is found.
+         *  Returns an Error if an Exception is thrown.
+         *  */
+        fun mostFrequent(watermarks: List<Watermark>): Result<List<Watermark>> {
+            var result: Result<List<Watermark>> = Result.success(watermarks)
+            if (watermarks.isEmpty()) {
+                return result
+            }
+            val frequencyMap: Map<Watermark, Int>
+            val maxFrequency: Int
+            val mostFrequent = mutableListOf<Watermark>()
+            val filteredList = mutableListOf<Watermark>()
+
+            try {
+                frequencyMap = watermarks.groupingBy { it }.eachCount()
+                maxFrequency = frequencyMap.maxOf { it.value }
+                mostFrequent.addAll(frequencyMap.filter { it.value == maxFrequency }.keys)
+                for (watermark in mostFrequent) {
+                    for (i in 1..maxFrequency) {
+                        filteredList.add(watermark)
+                    }
+                }
+                result =
+                    if (mostFrequent.size > 1) {
+                        MultipleMostFrequentWarning(mostFrequent.size).into(filteredList)
+                    } else {
+                        Result.success(filteredList)
+                    }
+            } catch (e: Exception) {
+                result =
+                    FrequencyAnalysisError(
+                        e::class.simpleName ?: "Unknown Exception",
+                    ).into(emptyList())
+            } finally {
+                return result
+            }
         }
     }
 
@@ -40,4 +84,18 @@ open class Watermark(var watermarkContent: List<Byte>) {
 
     /** Exposes content.hashCode() */
     override fun hashCode(): Int = watermarkContent.hashCode()
+
+    class MultipleMostFrequentWarning(
+        private val WatermarkCount: Int,
+    ) : Event.Warning("$SOURCE.mostFrequent") {
+        /** Returns a String explaining the event */
+        override fun getMessage() = "$WatermarkCount most frequent watermarks found!"
+    }
+
+    class FrequencyAnalysisError(
+        private val type: String,
+    ) : Event.Error("$SOURCE.mostFrequent") {
+        /** Returns a String explaining the event */
+        override fun getMessage(): String = "Frequency analysis failed with exception: $type!"
+    }
 }
